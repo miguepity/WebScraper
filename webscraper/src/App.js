fe6fe75ca7924c5ef19dcace37083e5eb27db695
miguepity <miguel.ardon@unitec.edu>
2018-08-29 22:01:02 -0600

    poco pero ahi vamos

---
+++ webscraper/src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import Scraper from './WebScraper/scraper';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const navbarInstance = (
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#brand">React-Bootstrap</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="#">
+              Link
+      </NavItem>
+            <NavItem eventKey={2} href="#">
+              Link
+      </NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">
+              Link Right
+      </NavItem>
+            <NavItem eventKey={2} href="#">
+              Link Right
+      </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+    return (
+      <Router >
+        <div className="App">
+          {navbarInstance}
+          <Route exact path="/" render={() => <Redirect to="#" />}></Route>
+        </div>
+      </Router>
+    );
+  }
+};
+
+export default App;
---
+++ webscraper/src/WebScraper/scraper.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class scraper extends Component{
+    constructor(){
+        super();
+        this.state = ({
+
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <p>Holla mundo</p>   
+            </div>
+        );
+    }
+}
+
+export default scraper;
